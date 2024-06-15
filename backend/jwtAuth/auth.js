101155d9f233a4bdbda68b8febdd279c485fe5d8
AdityaKumarSingh101 <31300740+AdityaKumarSingh101@users.noreply.github.com>
2024-06-16 05:03:26 +0530

    Added jwt auth

--- backend/jwtAuth/auth.js
+++ backend/jwtAuth/auth.js
@@ -1,1 +1,26 @@
 const jwt = require("jsonwebtoken");
+const secret = process.env.secret;
+
+const auth = async (req, res, next) => {
+  const token = req.header("x-auth-token");
+
+  if (!token) {
+    return res.status(401).json({ msg: "JWT Auth failed! No token." });
+  } else {
+    try {
+      await jwt.verify(token, secret, (error, decoded) => {
+        if (error) {
+          return res.status(401).json({ msg: "Token verification failed!" });
+        } else {
+          req.user = decoded.user;
+          next();
+        }
+      });
+    } catch (err) {
+      console.error("Auth Error!");
+      res.status(500).json({ msg: "Server Error!" });
+    }
+  }
+};
+
+module.exports = auth;
